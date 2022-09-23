45543993a69398d6a43be667493990c4f4853508
artemYakusha <111080434+artemYakusha@users.noreply.github.com>
2022-09-23 16:52:45 +0300

    лаб 3,4,5

---
+++ lab5.1.py
@@ -0,0 +1,19 @@
+import math as m
+bar = float(input('Введіть число х:  '))
+def f1():
+    rez = 2.27 * m.e ** (4*bar+1) + 3
+    return rez
+def f2():
+    rez = 0.64 * bar ** (bar+1)
+    return rez
+def f3():
+    rez = m.log(bar) * m.fabs(bar - m.e ** bar)
+    return rez
+y = 0.0
+if bar > 7:
+    y = f1()
+elif 0.5 < bar <= 7:
+    y = f2()
+else:
+    y = f3()
+print(int(y))
